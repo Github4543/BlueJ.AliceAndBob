4926c30a31fb39c731f7658fe0ce0876f9ca8378
Corey Williams <cwilliams@zipcodes-MacBook-Pro.local>
2020-02-05 22:28:21 -0500

    completed program

--- AliceAndBobEngine.java
+++ AliceAndBobEngine.java
@@ -14,9 +14,39 @@ public class AliceAndBobEngine
     }
 
     //HINT: LOOK AT THE TESTS TO SEE WHAT IS EXPECTED FOR DIFFERENT CONDITIONS
-    public String evaluateAliceOrBob(String input)
-    {
+    public String evaluateAliceOrBob(String input){
     
-        return result;
+    if (input.equals("Alice")){
+    return "Hello " + input;
     }
+    else if (input.equals("Alicia") || input.equals("Bobert")){
+    return "I DON'T KNOW YOU, YOU ARE SCARY"; 
+    }
+    else if (input.equals("Bob")){
+    return "Hello " + input;
+    }
+    else {
+        return "No good input";
+    }
+       
+   }
 }
+
+       /*AliceAndBobEngine aliceAndBobEngineTest = new AliceAndBobEngine();
+        
+        assertEquals("Hello Bob", aliceAndBobEngineTest.evaluateAliceOrBob("Bob"));
+        assertNotEquals("Hello Alice", aliceAndBobEngineTest.evaluateAliceOrBob("Pamela. Pamela the tropical citrus tree."));
+        
+        assertEquals("I DON'T KNOW YOU, YOU ARE SCARY", aliceAndBobEngineTest.evaluateAliceOrBob("Bobert"));
+        assertNotEquals("I DON'T KNOW YOU, YOU ARE SCARY", aliceAndBobEngineTest.evaluateAliceOrBob("Bob"));/*
+
+
+
+   /* AliceAndBobEngine aliceAndBobEngineTest = new AliceAndBobEngine();
+        
+        assertEquals("Hello Alice", aliceAndBobEngineTest.evaluateAliceOrBob("Alice"));
+        assertNotEquals("Hello Alice", aliceAndBobEngineTest.evaluateAliceOrBob("Jeff. Jeff the Elephant."));
+        
+        assertEquals("I DON'T KNOW YOU, YOU ARE SCARY", aliceAndBobEngineTest.evaluateAliceOrBob("Alicia"));
+        assertNotEquals("I DON'T KNOW YOU, YOU ARE SCARY", aliceAndBobEngineTest.evaluateAliceOrBob("Alice"));
+        */
